d99629917a3a716f1d4a20df7400f08274102cb3
Dima <smolentsev_dn@icloud.com>
2025-04-30 11:05:36 +0200

    feat: add resolution select

---
+++ src/components/ResolutionSelect.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+import BaseSelect from './BaseSelect.vue'
+import { defineProps, defineEmits, computed } from 'vue'
+
+export type Resolution = {
+  width: number
+  height: number
+}
+
+export type ResolutionOption = {
+  label: string
+  value: Resolution
+}
+
+const RESOLUTION_OPTIONS: ResolutionOption[] = [
+  {
+    value: { width: 1920, height: 1080 },
+    label: '1920x1080 16:9',
+  },
+  {
+    value: { width: 1080, height: 1920 },
+    label: '1080x1920 9:16',
+  },
+  {
+    value: { width: 640, height: 480 },
+    label: '640x480 4:3',
+  },
+  {
+    value: { width: 600, height: 600 },
+    label: '600x600 1:1',
+  },
+]
+
+const { modelValue } = defineProps<{
+  modelValue: Resolution
+}>()
+
+const emit = defineEmits<{
+  'update:modelValue': [resolution: Resolution]
+}>()
+
+const resolution = computed({
+  get: () => modelValue,
+  set: (resolution: Resolution) => emit('update:modelValue', resolution),
+})
+</script>
+
+<template>
+  <BaseSelect v-model="resolution" label="Resolution" :options="RESOLUTION_OPTIONS" />
+</template>
