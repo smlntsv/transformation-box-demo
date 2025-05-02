381813d35d685dc8f882996d1ba5e5f3aab44240
Dima <smolentsev_dn@icloud.com>
2025-05-02 11:02:50 +0200

    feat: add viewport class

---
+++ src/canvas/viewport.ts
@@ -0,0 +1,58 @@
+import { debounce } from '../utils/debounce.ts'
+
+class Viewport {
+  private canvas: HTMLCanvasElement
+  private observer: ResizeObserver | null = null
+  private readonly dpr: number
+  private readonly onResizeCallback?: () => void
+
+  constructor(canvas: HTMLCanvasElement, onResizeCallback?: () => void) {
+    this.onParentResize = this.onParentResize.bind(this)
+    this.canvas = canvas
+    this.dpr = typeof window !== 'undefined' ? window.devicePixelRatio : 1
+
+    if (onResizeCallback) {
+      this.onResizeCallback = onResizeCallback
+    }
+
+    const parentElement = this.canvas.parentElement
+
+    if (parentElement) {
+      this.observer = new ResizeObserver(
+        debounce((entries: ResizeObserverEntry[]) => {
+          if (entries.length) {
+            this.onParentResize(entries[0].contentRect)
+
+            if (this.onResizeCallback) {
+              this.onResizeCallback()
+            }
+          }
+        })
+      )
+
+      this.observer.observe(parentElement)
+    }
+  }
+
+  public destroy() {
+    if (this.observer) {
+      this.observer.disconnect()
+      this.observer = null
+    }
+  }
+
+  public applyTransform(ctx: CanvasRenderingContext2D) {
+    ctx.scale(this.dpr, this.dpr)
+  }
+
+  private onParentResize(parentContentRect: DOMRectReadOnly) {
+    const width = Math.floor(parentContentRect.width)
+    const height = Math.floor(parentContentRect.height)
+    this.canvas.width = width * this.dpr
+    this.canvas.height = height * this.dpr
+    this.canvas.style.width = width + 'px'
+    this.canvas.style.height = height + 'px'
+  }
+}
+
+export { Viewport }
